c019f569ad0309a7e62be6cd2f888981623d823a
AbdurRahamn M Zaghloul <drabdohza@gmail.com>
2025-09-29 11:44:32 +0300

    Hooking up the register method to the service

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -10,11 +11,18 @@ import { FormsModule } from '@angular/forms';
 })
 export class RegisterComponent {
   model: any = {};
-  userFromHomeCmp = input.required<any>();
   cancelRegister = output<boolean>();
+  accountService = inject(AccountService);
 
   register() {
-    console.log(this.model);
+    this.accountService.Register(this.model).subscribe({
+      next: res => {
+        this.cancel();
+        console.log(res);
+      },
+      error: err => console.log(err),
+      complete: () => console.log('request completed')
+    })
   }
 
   cancel() {
