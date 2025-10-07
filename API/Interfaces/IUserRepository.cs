389bb790ae55f7224c5a31a4c436675f0744f2c4
AbdurRahamn M Zaghloul <drabdohza@gmail.com>
2025-10-07 11:24:36 +0300

    using AutoMapper querable projection to optimize DB queries

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -11,4 +11,6 @@ public interface IUserRepository
     public Task<IEnumerable<AppUser>> GetUsersAsync();
     Task<AppUser?> GetUserByIdAsync(int id);
     Task<AppUser?> GetUserByUsernameAsync(string username);
+    Task<MemberDto?> GetMemberAsync(string username);
+    Task<IEnumerable<MemberDto>> GetMembersAsync();
 }
