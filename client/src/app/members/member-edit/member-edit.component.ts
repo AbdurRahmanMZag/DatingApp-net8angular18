7b2eec87e9f670accc708d98aa787b6f5916a24a
AbdurRahamn M Zaghloul <drabdohza@gmail.com>
2025-10-15 10:04:41 +0300

    Enhance member edit component with form validation and update functionality

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
@@ -13,16 +14,27 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './member-edit.component.css'
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
+  member?: Member;
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
-  member?: Member;
+  private toastr = inject(ToastrService);
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  private loadMember() {
     const user = this.accountService.currentUser();
-    if (user) {
-      this.memberService.getMember(user.username).subscribe({
-        next: member => this.member = member
-      })
-    }
+    if (!user) return;
+    this.memberService.getMember(user.username).subscribe({
+      next: member => this.member = member
+    });
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
   }
 }
